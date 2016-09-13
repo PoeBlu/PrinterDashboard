c06aa3061880df3b12dad34c5dc05e1368a7503d
Seth Braunstein <sethbraunstein@gmail.com>
2016-09-13 13:27:10 -0400

    Powershell Printer Dashboard
    Updated script with documentation so people like me can understand it.

--- DashBoard.ps1
+++ DashBoard.ps1
@@ -1,22 +1,23 @@
-﻿#Add to first line of the script
-
+﻿#add to first line of the script for loop
 For(;;){ 
  
 
+#clear all variables
 $main = $Null 
+#define print servers
 $PrintServers = "PrintServer1","PrintServer2","PrintServer3","PrintServer4"
+#pull printers from all servers
 $PrintersUnsorted = ForEach ($PrintServer in $PrintServers){
+        #Mobile Printers all contain ROV in the name. Filtering those out to get rid of false positives as they are powered up on demand.
         get-printer -cn $printServer | where Name -notlike '*ROV*'
         }
-$Printers = $PrintersUnsorted | sort PrinterSTatus
+#$Printers is sorted by print server at this point. Sorting to group by printer name
+$Printers = $PrintersUnsorted | sort PrinterStatus
 $head = (Get-Content c:\install\printerdashboard\head.html) -replace '%4',(get-date).DateTime
 $tail = Get-Content c:\install\printerdashboard\tail.html
 
-
+#loop through each printer and discard any printers that are normal or tonerlow
 ForEach ($Printer in $Printers | Where-Object { $_.printerstatus -ne "normal" -and $_.printerstatus -ne "tonerlow"}){
-
-    #increment $i, to use the many different background image options
-    $i++
 
     #set the name 
     $Name=$Printer.Name
@@ -42,7 +43,9 @@ ForEach ($Printer in $Printers | Where-Object { $_.printerstatus -ne "normal" -a
               $($Printer.Printerstatus)<br>
               $($Printer.ComputerName)"
     }
+    #assign variable to create link for tile
     $IPAddress="http://"+$Printer.PortName
+    #assign variable to show IP on mouseover of tile
     $IP=$Printer.Portname
 
     #make a card
@@ -65,7 +68,8 @@ $main += $tile
 }
 $html = $head + $main + $tail
 
+#place html report 
 $html > c:\install\printerdashboard\PTRReport.html
 
-#Last line of script
+#last line of script to loop
 Start-Sleep -Seconds 90}
